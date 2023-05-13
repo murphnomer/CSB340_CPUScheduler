133a01bcbc219bf923df22604356ebaa6c32938b
Jared Scarr <jaredscarr@pm.me>
2023-05-13 14:26:48 -0700

    Remove comment.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
     private static final boolean DISPLAY_MODE = true;
 
     public static void main(String[] args) {
-        // TODO: add toggle for each algorithm to not have the display printed
         TestUtil util = new TestUtil();
         Process[] defaultData = util.getDefaultTestData();
         ScheduleInterface[] algorithms = {new RR(Arrays.stream(defaultData).toList()), new SJF(defaultData)};
