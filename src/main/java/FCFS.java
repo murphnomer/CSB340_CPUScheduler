81f48e6bb462201676c11c5a55c0594dfe6f1d48
derrekdo <derrekdo123@gmail.com>
2023-05-14 17:02:54 -0700

    updated FCFS

--- src/main/java/FCFS.java
+++ src/main/java/FCFS.java
@@ -12,21 +12,22 @@ public class FCFS implements ScheduleInterface{
     private List<Process> inIO = new LinkedList<>();
     private List<Process> completed;
     private List<Process> processes;
-    int size;
+    private int size;
     private int currentTime;
-    private int totWaitTime;
-    private int totTurnaroundTime;
-    private int totResponseTime;
+    private double cpuTime = 0;
+    private double totWaitTime;
+    private double totTurnaroundTime;
+    private double totResponseTime;
     private Process current;
-
+    boolean displayMode;
 
     public FCFS(List<Process> readyQueue) {
         inReadyQueue = new LinkedList<>(readyQueue);
         processes = new LinkedList<>(readyQueue);
         size = inReadyQueue.size();
         completed = new ArrayList<>(size);
-
         currentTime = 0;
+        displayMode = false;
     }
 
     public List<Process> process() {
@@ -42,26 +43,20 @@ public class FCFS implements ScheduleInterface{
                 }
                 for (Process process : inIO) {
                     burstDuration = Math.min(burstDuration, process.getCurrentDuration());
-
                 }
                 //TODO: make IO time decrease while no running process
             }
-
-            display();
-            runProcess(burstDuration);
-//            System.out.println("test");
+//            display();
+//            displayState(true);
+            if (displayMode) {
+                displayState(false);
+            }
+            tickProcess(burstDuration);
         }
-        display();
-
-
+        if (displayMode) {
+            displayState(false);
+        }
         return completed;
-    }
-
-
-
-    public void runProcess (int burstDuration) {
-//        current.setCurrentState(Process.State.RUNNING);
-        tickProcess(burstDuration);
     }
 
     public void tickProcess(int burstDuration) {
@@ -77,6 +72,10 @@ public class FCFS implements ScheduleInterface{
                     inIO.remove(current);
                 } else if (current.getCurrentState() == Process.State.FINISHED && !completed.contains(current)) {
                     completed.add(current);
+                    totWaitTime += current.getWaitingTime();
+                    totTurnaroundTime += current.getTurnaroundTime();
+                    totResponseTime += current.getResponseTime();
+                    cpuTime += current.getCpuTime();
                 }
             }
             burstDuration--;
@@ -85,66 +84,192 @@ public class FCFS implements ScheduleInterface{
     }
 
     public void display() {
-        System.out.println("Current Time: " + currentTime);
+        System.out.println("\n\nCurrent Time: " + currentTime);
         System.out.print("Next Process on CPU: ");
         if (current != null && current.getCurrentState() == Process.State.RUNNING) {
-            System.out.println(current.getName() + " Burst: " + current.getCurrentDuration());
+            System.out.println(current.getName() + " \nBurst Time: " + current.getCurrentDuration());
         } else {
             System.out.println("IDLE");
         }
 
-        System.out.println("---------------------------");
-        System.out.println("In Ready Queue: ");
+        System.out.println("..................................................");
+        System.out.println("\nList of processes in the ready queue:\n");
+        System.out.println("\t\tProcess\t\tBurst");
         if (inReadyQueue.isEmpty()) {
-            System.out.println("empty");
+            System.out.println("\t\t[empty]");
         } else {
             for (Process p : inReadyQueue) {
-                System.out.println(p.getName() + " " + p.getCurrentDuration());
+                System.out.println("\t\t\t" + p.getName() + "\t\t\t" + p.getCurrentDuration());
             }
             System.out.println();
         }
-        System.out.println("---------------------------");
-        System.out.println("In IO");
+        System.out.println("..................................................");
+        System.out.println("\nList of processes in I/O:\n");
+        System.out.println("\t\tProcess\t\tRemaining I/O time");
         if (inIO.isEmpty()) {
-            System.out.println("empty");
+            System.out.println("\t\t[empty]");
         } else {
             for (Process p : inIO) {
-                System.out.println(p.getName() + " " + p.getCurrentDuration());
+                System.out.println("\t\t\t" + p.getName() + "\t\t\t" + p.getCurrentDuration());
             }
         }
-        System.out.println("--------------------------");
+        System.out.println("..................................................");
         if (!completed.isEmpty()) {
-            System.out.print("Completed: ");
+            System.out.print("\nCompleted: ");
             for (Process p : completed) {
                 System.out.print(p.getName() + " ");
             }
-            System.out.println("\n----------------------------");
+            System.out.println();
+            System.out.println("..................................................");
         }
+        System.out.println("..................................................");
 
         if (completed.size() == size) {
-            System.out.println("\n\n\n");
-            for (Process p : completed) {
-                System.out.print(p.getName() + " ");
-                System.out.print(p.getTotalTime() + " ");
-                System.out.print(p.getWaitingTime() + " ");
-                System.out.print(p.getIoTime() + " ");
-                System.out.println(p.getCpuTime());
+            Queue<Integer> waitTimes = new LinkedList<>();
+            Queue<Integer> turnAroundTimes = new LinkedList<>();
+            Queue<Integer> responseTimes = new LinkedList<>();
 
+            System.out.println("\n\n");
+            System.out.println("FINISHED\n");
+            System.out.println("Total Time:\t\t\t" + currentTime);
+            System.out.printf("CPU Utilization:\t%.4f", (cpuTime / currentTime) * 100 );
+            System.out.println("%");
+            String[] timeType = {"Waiting Times", "Turnaround Times", "Response Times"};
 
+            for (int i = 0; i < timeType.length; i++) {
+                System.out.print("\n" + timeType[i] + "\t");
+                if (i != 1) {
+                    System.out.print("\t");
+                }
+
+                for (Process process : processes) {
+                    System.out.print(process.getName() + "\t");
+                    if (i == 0) {
+                        waitTimes.add(process.getWaitingTime());
+                        turnAroundTimes.add(process.getTurnaroundTime());
+                        responseTimes.add(process.getResponseTime());
+                    }
+                }
+                System.out.print("\n\t\t\t\t\t");
+                if (i == 0) {
+                    while (!waitTimes.isEmpty()) {
+                        System.out.print(waitTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Wait:\t\t%.2f %n", (totWaitTime / size));
+                } else if (i == 1) {
+                    while (!turnAroundTimes.isEmpty()) {
+                        System.out.print(turnAroundTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Turnaround: %.3f %n", (totTurnaroundTime / size));
+                } else {
+                    while (!responseTimes.isEmpty()) {
+                        System.out.print(responseTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Response:\t%.3f %n%n", (totResponseTime / size));
+                }
             }
-//            System.out.println("FINISHED\n");
-//            System.out.println("Total Time: " + currentTime);
-//            System.out.println("CPU Utilization: " + current.getCpuTime());
         }
     }
 
     @Override
     public void displayState(boolean waitBetweenPages) {
+        System.out.println("\n\nCurrent Time: " + currentTime);
+        System.out.print("Next Process on CPU: ");
+        if (current != null && current.getCurrentState() == Process.State.RUNNING) {
+            System.out.println(current.getName() + " \nBurst Time: " + current.getCurrentDuration());
+        } else {
+            System.out.println("IDLE");
+        }
 
+        System.out.println("..................................................");
+        System.out.println("\nList of processes in the ready queue:\n");
+        System.out.println("\t\tProcess\t\tBurst");
+        if (inReadyQueue.isEmpty()) {
+            System.out.println("\t\t[empty]");
+        } else {
+            for (Process p : inReadyQueue) {
+                System.out.println("\t\t\t" + p.getName() + "\t\t\t" + p.getCurrentDuration());
+            }
+            System.out.println();
+        }
+        System.out.println("..................................................");
+        System.out.println("\nList of processes in I/O:\n");
+        System.out.println("\t\tProcess\t\tRemaining I/O time");
+        if (inIO.isEmpty()) {
+            System.out.println("\t\t[empty]");
+        } else {
+            for (Process p : inIO) {
+                System.out.println("\t\t\t" + p.getName() + "\t\t\t" + p.getCurrentDuration());
+            }
+        }
+        System.out.println("..................................................");
+        if (!completed.isEmpty()) {
+            System.out.print("\nCompleted: ");
+            for (Process p : completed) {
+                System.out.print(p.getName() + " ");
+            }
+            System.out.println();
+            System.out.println("..................................................");
+        }
+        System.out.println("..................................................");
+
+        if (completed.size() == size) {
+            Queue<Integer> waitTimes = new LinkedList<>();
+            Queue<Integer> turnAroundTimes = new LinkedList<>();
+            Queue<Integer> responseTimes = new LinkedList<>();
+
+            System.out.println("\n\n");
+            System.out.println("FINISHED\n");
+            System.out.println("Total Time:\t\t\t" + currentTime);
+            System.out.printf("CPU Utilization:\t%.4f", (cpuTime / currentTime) * 100 );
+            System.out.println("%");
+            String[] timeType = {"Waiting Times", "Turnaround Times", "Response Times"};
+
+            for (int i = 0; i < timeType.length; i++) {
+                System.out.print("\n" + timeType[i] + "\t");
+                if (i != 1) {
+                    System.out.print("\t");
+                }
+
+                for (Process process : processes) {
+                    System.out.print(process.getName() + "\t");
+                    if (i == 0) {
+                        waitTimes.add(process.getWaitingTime());
+                        turnAroundTimes.add(process.getTurnaroundTime());
+                        responseTimes.add(process.getResponseTime());
+                    }
+                }
+                System.out.print("\n\t\t\t\t\t");
+                if (i == 0) {
+                    while (!waitTimes.isEmpty()) {
+                        System.out.print(waitTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Wait:\t\t%.2f %n", (totWaitTime / size));
+                } else if (i == 1) {
+                    while (!turnAroundTimes.isEmpty()) {
+                        System.out.print(turnAroundTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Turnaround: %.3f %n", (totTurnaroundTime / size));
+                } else {
+                    while (!responseTimes.isEmpty()) {
+                        System.out.print(responseTimes.remove() + "\t");
+                    }
+                    System.out.printf("\nAverage Response:\t%.3f %n%n", (totResponseTime / size));
+                }
+            }
+        }
+        if (waitBetweenPages) {
+            System.out.println("Press Enter to continue . . .");
+            try {
+                System.in.read();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
     @Override
     public void setDisplayMode(boolean displayMode) {
-
+        this.displayMode = displayMode;
     }
 }
--- src/test/java/FCFSTest.java
+++ src/test/java/FCFSTest.java
@@ -3,19 +3,23 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class FCFSTest {
 
     @Test
     void fcfsTest() {
         TestUtil testUtil = new TestUtil();
+        String[] expected = {"P1", "P6", "P8", "P7", "P5", "P3", "P2", "P4"};
         Process[] data = testUtil.getDefaultTestData();
         FCFS fcfs = new FCFS(Arrays.asList(data));
+        fcfs.setDisplayMode(true);
         List<Process> result = fcfs.process();
         String[] actual = new String[result.size()];
         for (int i = 0; i < result.size(); i++) {
             actual[i] = result.get(i).getName();
         }
-        String[] expected = {"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"};
+        assertArrayEquals(expected, actual);
     }
 
 }
