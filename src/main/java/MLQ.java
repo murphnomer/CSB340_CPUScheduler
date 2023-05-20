17af55685d02f1dd9b10f7406a0156fcc9531730
Mike Murphy <elmiko@gmail.com>
2023-05-20 14:06:11 -0700

    MLQ start

--- src/main/java/MLQ.java
+++ src/main/java/MLQ.java
@@ -1,2 +1,54 @@
-public class MLQ {
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MLQ implements ScheduleInterface {
+    // priority queue of processes waiting for CPU time
+    RR foregroundQueue;
+    FCFS backgroundQueue;
+    // list of all processes regardless of status
+    List<Process> allProcesses;
+    // list of processes currently doing IO
+    List<Process> outForIO;
+    // list of processes that have already finished their work
+    List<Process> finishedProcesses;
+    // tracking variable for how many CPU ticks have elapsed
+    int timer;
+    // total number of processes
+    int totalNumberOfProcesses;
+    // counter for how long the CPU has been idle
+    int idleCPUTime;
+    // indicator of whether the CPU is currently idle
+    boolean cpuIsIdle;
+    // pointer to the process currently executing on the CPU
+    Process procOnCPU;
+    // switch variable indicating whether to display state at every context switch
+    private boolean displayMode = false;
+
+    public
+
+    @Override
+    public List<Process> process() {
+        return null;
+    }
+
+    @Override
+    public void displayState(boolean waitBetweenPages) {
+
+    }
+
+    @Override
+    public void setDisplayMode(boolean displayMode) {
+
+    }
+
+    @Override
+    public int getTotalElapsedTime() {
+        return 0;
+    }
+
+    @Override
+    public int getTotalIdleCPUTime() {
+        return 0;
+    }
 }
